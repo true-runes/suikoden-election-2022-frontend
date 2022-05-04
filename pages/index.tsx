62cbe48932b5e738f1857f6fdeb0e172853d1191
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-05 01:51:39 +0900

    feat: 🎸 カウントダウンのひな形を追加した (#145)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
 import type { NextPage } from 'next'
 import Image from 'next/image'
 
@@ -9,7 +12,33 @@ import KaihyoIllustStatus from '../components/KaihyoIllustStatus'
 import TouhyouCheck from '../components/TouhyouCheck'
 import SiteFooter from '../components/SiteFooter'
 
+const nowSecond = () => {
+  const time = new Date()
+  const hour = time.getHours()
+  const minute = time.getMinutes()
+  const second = time.getSeconds()
+
+  return second
+}
+
 const Home: NextPage = () => {
+  // TODO: "now" ではなく、開催日からの差分を出し、減らしていく
+  const [now, setNow] = useState(new Date())
+
+  useEffect(
+    function () {
+      const intervalId = setInterval(function () {
+        setNow(new Date())
+      }, 1000)
+
+      return function () {
+        clearInterval(intervalId)
+      }
+    },
+
+    [now]
+  )
+
   return (
     // tailwind の container でくくる
     <div>
@@ -30,6 +59,42 @@ const Home: NextPage = () => {
           height={4320}
         />
 
+        <div className="hero bg-base-200">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <span className="text-xl">投票開始まであと</span>
+              <br />
+              <br />
+              <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <StyledNowDate now={now} />
+                  </span>
+                  日
+                </div>
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <StyledNowHour now={now} />
+                  </span>
+                  時
+                </div>
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <StyledNowMinute now={now} />
+                  </span>
+                  分
+                </div>
+                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                  <span className="countdown font-mono text-5xl">
+                    <StyledNowSecond now={now} />
+                  </span>
+                  秒
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="divider" />
         <KaishiNichizi />
         <div className="divider" />
@@ -48,3 +113,19 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+const StyledNowSecond = styled.span`
+  --value: ${(props: { now: Date }) => props.now.getSeconds()};
+`
+
+const StyledNowMinute = styled.span`
+  --value: ${(props: { now: Date }) => props.now.getMinutes()};
+`
+
+const StyledNowHour = styled.span`
+  --value: ${(props: { now: Date }) => props.now.getHours()};
+`
+
+const StyledNowDate = styled.span`
+  --value: ${(props: { now: Date }) => props.now.getDate()};
+`

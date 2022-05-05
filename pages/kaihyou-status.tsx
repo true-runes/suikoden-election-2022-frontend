1cf2b0b4a479447680be615cfa94441a81a846ba
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-05 18:22:04 +0900

    feat: 🎸 Grid.js を導入した (#148)
    * feat: 🎸 Grid.js を導入した
    
    $ yarn add -dev gridjs gridjs-react
    
    * feat: 🎸 イラスト投稿状況に Grid.js を仮導入した

--- pages/kaihyou-status.tsx
+++ pages/kaihyou-status.tsx
@@ -1,8 +1,35 @@
+import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import HumbergerMenu from '../components/HumbergerMenu'
+import { Grid } from 'gridjs'
 
 const KaihyoStatus: NextPage = () => {
+  const wrapperRef: any = React.useRef(null)
+
+  const grid = new Grid({
+    columns: ['キャラ名', '応募数'],
+    search: {
+      enabled: true,
+    },
+    sort: true,
+    data: [
+      ['ルック', 543],
+      ['テッド', 466],
+      ['フリック', 466],
+    ],
+    className: {
+      th: 'text-xl',
+      td: 'bg-primary bg-base-200 text-2xl',
+      table: 'bg-base-200 bg-neutral',
+    },
+    style: {},
+  })
+
+  useEffect(() => {
+    grid.render(wrapperRef.current)
+  })
+
   return (
     <div>
       <title>開票イラスト応募状況 - 幻水総選挙2022</title>
@@ -14,33 +41,16 @@ const KaihyoStatus: NextPage = () => {
       <main id="page-wrap">
         <div className="navbar bg-neutral text-neutral-content">
           <div className="normal-case text-xl" style={{ paddingLeft: '1em' }}>
-            <Link href="/">幻水総選挙2022</Link>
+            <Link href="/">開票イラスト応募状況</Link>
           </div>
         </div>
 
+        <div className="flex justify-center text-2xl px-2 py-2">
+          2022/05/06 12:00 現在
+        </div>
+
         <div className="overflow-x-auto">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th>キャラ名</th>
-                <th>応募数</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>ルック</td>
-                <td>543</td>
-              </tr>
-              <tr>
-                <td>テッド</td>
-                <td>466</td>
-              </tr>
-              <tr>
-                <td>フリック</td>
-                <td>466</td>
-              </tr>
-            </tbody>
-          </table>
+          <div ref={wrapperRef} className="px-2" />
         </div>
       </main>
     </div>

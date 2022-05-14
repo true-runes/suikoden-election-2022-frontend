72cb4a3b05c84d1d4b747f327067a75bc92a4c7b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-14 19:12:58 +0900

    feat: 🎸 Google Analytics (Google Tag Manager) を実装した (#191)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { googleTagManagerId } from '../utils/gtm'
+import GoogleTagManager, {
+  GoogleTagManagerId,
+} from '../components/GoogleTagManager'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleTagManager
+        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

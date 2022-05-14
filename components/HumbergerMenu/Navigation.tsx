69b985ce1b19592b7ae96b8c15f2c9287b4ab6bf
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-14 23:28:27 +0900

    feat: 🎸 20220514リリースにあたり不要なコードを削除した (#203)

--- components/HumbergerMenu/Navigation.tsx
+++ components/HumbergerMenu/Navigation.tsx
@@ -12,15 +12,6 @@ const Navigation = () => {
         <div className="divide-neutral-50">
           <div className="divider"></div>
         </div>
-        <div className="text-lg">
-          <Link href="/how-to-vote">📝 投票方法</Link>
-        </div>
-        <div className="text-lg">
-          <Link href="/kikaku">💡 企画</Link>
-        </div>
-        <div className="text-lg">
-          <Link href="/faq">❓ よくある質問</Link>
-        </div>
         <PreviousWebsites />
       </Menu>
     </div>

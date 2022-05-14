712a6df0d872ffec231756b0b4b6375fc9a88c85
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-14 23:08:23 +0900

    feat: 🎸 「お願い」コンポーネントを追加した

---
+++ components/Onegai.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next'
+
+const Onegai: NextPage = () => {
+  return (
+    <div className="bg-white text-black">
+      <div className="hero">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-2xl font-bold pb-4">お願い</h1>
+            <div className="text-base text-left pb-4">
+              <ul className="list-disc pl-6 pr-2">
+                <li className="pb-2">投票は各部門につき、お一人様1回です</li>
+                <li className="pb-2">
+                  ツイート投票、あるいはDM投票のどちらかでのみ投票できます
+                </li>
+                <li className="pb-2">
+                  明らかに同じ方からの複数投票がありました場合は、無効にさせて頂く可能性がございます。
+                  <br />
+                  推しへの清き投票にご協力よろしくお願いいたします！一生のお願いだよ
+                </li>
+                <li className="pb-2">
+                  総選挙関連のハッシュタグで、特定のキャラや特定のキャラを推す方への批判・悪口を目的としたツイートは、ご遠慮下さいますようお願い致します
+                </li>
+                <li className="pb-2">
+                  総選挙関連のハッシュタグで、カップリング要素を含むツイートは、公式程度の表現として頂けますと幸いです
+                </li>
+                <li className="pb-2">
+                  なるべく制限は設けたくないと思いますが、多くの方にご参加頂いておりますため、ご理解とご協力を頂けますようお願い致します。
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Onegai

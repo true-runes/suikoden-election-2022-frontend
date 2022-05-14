50dab332121a0e31fcd97ef65fa1df09461dd4b6
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-14 23:53:12 +0900

    fix: 🐛 Hotfix Middleware 記述バグ修正

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,27 +1,23 @@
-// import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-// export const middleware = (req: NextRequest) => {
-//   if (process.env.DEPLOYMENT_ENVIRONMENT === 'production')
-//     return NextResponse.next()
-
-//   const basicAuth = req.headers.get('authorization')
-
-//   if (basicAuth) {
-//     const auth = basicAuth.split(' ')[1]
-//     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-
-//     if (
-//       user === process.env.BASIC_AUTH_USERNAME &&
-//       pwd === process.env.BASIC_AUTH_PASSWORD
-//     ) {
-//       return NextResponse.next()
-//     }
-//   }
-
-//   return new Response('Auth required', {
-//     status: 401,
-//     headers: {
-//       'WWW-Authenticate': 'Basic realm="Secure Area"',
-//     },
-//   })
-// }
+export const middleware = (req: NextRequest) => {
+  // if (process.env.DEPLOYMENT_ENVIRONMENT === 'production')
+  //   return NextResponse.next()
+  // const basicAuth = req.headers.get('authorization')
+  // if (basicAuth) {
+  //   const auth = basicAuth.split(' ')[1]
+  //   const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+  //   if (
+  //     user === process.env.BASIC_AUTH_USERNAME &&
+  //     pwd === process.env.BASIC_AUTH_PASSWORD
+  //   ) {
+  //     return NextResponse.next()
+  //   }
+  // }
+  // return new Response('Auth required', {
+  //   status: 401,
+  //   headers: {
+  //     'WWW-Authenticate': 'Basic realm="Secure Area"',
+  //   },
+  // })
+}

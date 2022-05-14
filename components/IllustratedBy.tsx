175915ab5be04ecdfa3cf9430bf93be439c0e831
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-14 23:33:08 +0900

    feat: 🎸 リリース前の最終調整（テキストの削除等） (#205)

--- components/IllustratedBy.tsx
+++ components/IllustratedBy.tsx
@@ -3,19 +3,7 @@ import type { NextPage } from 'next'
 const IllustratedBy: NextPage = () => {
   return (
     <>
-      <div className="text-base text-center">
-        イラストご協力
-        <br />
-        まみた様(
-        <a
-          href="https://twitter.com/mamita404"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span className="underline">@mamita404</span>
-        </a>
-        )
-      </div>
+      <div className="text-base text-center">イラストご協力: まみた様</div>
     </>
   )
 }

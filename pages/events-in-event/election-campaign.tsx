3bd1a4dea0ab5ee47bc86fd2d72666dcbb08ebc3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 22:08:31 +0900

    feat: 🎸 「総選挙内企画」のページを20220515リリースにあわせて正式実装した

--- pages/events-in-event/election-campaign.tsx
+++ pages/events-in-event/election-campaign.tsx
@@ -31,13 +31,44 @@ const ElectionCampaign: NextPage = () => {
         <div className="hero">
           <div className="hero-content text-center">
             <div className="max-w-md">
-              <h1 className="text-2xl font-bold pb-4">① 選挙運動</h1>
+              <h1 className="text-2xl font-bold pb-4 underline font-zen-old-mincho">
+                選挙運動
+              </h1>
+              <h2 className="text-xl font-bold pb-4">選挙運動とは？</h2>
               <div className="text-base text-left">
+                <div className="pb-4">
+                  ハッシュタグ「{' '}
+                  <a
+                    href="https://twitter.com/search?q=%23%E5%B9%BB%E6%B0%B4%E7%B7%8F%E9%81%B8%E6%8C%99%E9%81%8B%E5%8B%95&src=typed_query&f=live"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="underline underline-offset-2 text-blue-500 hover:text-blue-900"
+                  >
+                    #幻水総選挙運動
+                  </a>{' '}
+                  」をつけて、選挙運動をしませんか？
+                  推しを応援するも良し。魅力や好きなシーンを語って布教するも良し。総選挙を盛り上げるも良し。ご自由にお使い下さい。
+                </div>
                 <ul className="list-disc pl-6 pr-2">
-                  <li className="pb-2">
-                    ハッシュタグ「 #幻水総選挙運動
-                    」をつけて、選挙運動をしませんか？
-                    推しを応援するも良し。魅力や好きなシーンを語って布教するも良し。総選挙を盛り上げるも良し。ご自由にお使い下さい。
+                  <li className="py-1">
+                    応援するキャラ名を本文中に記載していただけますと嬉しいです。
+                  </li>
+                  <li className="py-1">文字のみのツイートも大歓迎です！</li>
+                  <li className="py-1">
+                    ご自身の作品を添付していただいても構いません。
+                  </li>
+                  （恋愛描写を含む作品を添付する場合は、原作程度の表現でお願いします）
+                  <li className="py-1">
+                    「{' '}
+                    <a
+                      href="https://twitter.com/search?q=%23%E5%B9%BB%E6%B0%B4%E7%B7%8F%E9%81%B8%E6%8C%992022&src=typed_query&f=live"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="underline underline-offset-2 text-blue-500 hover:text-blue-900"
+                    >
+                      #幻水総選挙2022
+                    </a>{' '}
+                    」タグの使用は任意です。
                   </li>
                 </ul>
               </div>
--- pages/events-in-event/novels-on-themes.tsx
+++ pages/events-in-event/novels-on-themes.tsx
@@ -31,15 +31,72 @@ const NovelsOnThemes: NextPage = () => {
         <div className="hero">
           <div className="hero-content text-center">
             <div className="max-w-md">
-              <h1 className="text-2xl font-bold pb-4">① 選挙運動</h1>
-              <div className="text-base text-left">
+              <h1 className="text-2xl font-bold pb-4 underline font-zen-old-mincho">
+                お題小説
+              </h1>
+
+              <h2 className="text-xl font-bold pb-4">お題小説とは？</h2>
+              <div className="text-base text-left pb-4">
+                キャラとお題（テーマ）を選んで小説を書いていただき、ハッシュタグを付けてツイートする企画です。
+                イラストだけでなく文字書きの方も参加できる企画があれば…と思い、昨年より開始いたしました。
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">お題小説の「お題」募集</h2>
+              <div className="text-base text-left pb-4">
+                <p className="pb-2">
+                  企画に使わせていただくお題を募集します。
+                  単語やセリフなど、何でもOKです。
+                </p>
+                <p>
+                  お題は全部で3つで、そのうち1つは「フリー（内容自由）」となります。残りの2つを募集します。
+                </p>
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">募集期間</h2>
+              <div className="text-base text-center pb-4">
+                2022年5月15日（日）～5月21日（土）
+              </div>
+
+              <div className="divider" />
+
+              <h3 className="text-xl font-bold pb-4">応募方法</h3>
+              <div className="text-base text-left pb-4">
+                下記フォームよりお題を送信してください。
+                なお、ご送信いただきました内容について、個別のご連絡やご返信は行ないませんのでご了承ください。
+              </div>
+              <div className="text-base text-center pb-4">
+                <a
+                  href="https://ws.formzu.net/dist/S669729239/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline underline-offset-4 text-blue-500 hover:text-blue-900"
+                >
+                  お題応募用フォーム
+                </a>
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">昨年のお題</h2>
+              <div className="text-base text-left pb-4">
+                <div className="pb-4">昨年のお題は下記の3つでした。</div>
+
                 <ul className="list-disc pl-6 pr-2">
-                  <li className="pb-2">
-                    ハッシュタグ「 #幻水総選挙運動
-                    」をつけて、選挙運動をしませんか？
-                    推しを応援するも良し。魅力や好きなシーンを語って布教するも良し。総選挙を盛り上げるも良し。ご自由にお使い下さい。
-                  </li>
+                  <li className="pb-2">「祭」</li>
+                  <li className="pb-2">「どれにする？」</li>
+                  <li className="pb-2">（フリー）</li>
                 </ul>
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">お題小説の投稿について</h2>
+              <div className="text-base text-left">
+                小説作品の投稿方法などについては、後日発表いたします。
               </div>
             </div>
           </div>

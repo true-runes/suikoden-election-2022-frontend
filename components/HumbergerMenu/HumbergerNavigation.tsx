fc6131a4a4201cb608ffee9731b9cf56d918e90f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 20:56:14 +0900

    feat: 🎸 「総選挙内企画」説明ページのドラフトを作成 (#249)

--- components/HumbergerMenu/HumbergerNavigation.tsx
+++ components/HumbergerMenu/HumbergerNavigation.tsx
@@ -10,9 +10,53 @@ const Navigation = () => {
           <div className="text-2xl py-4">
             <Link href="/">幻水総選挙2022</Link>
           </div>
-          <div className="text-lg py-4">開催概要</div>
-          <div className="text-lg py-4">総選挙内企画</div>
-          <div className="text-lg py-4">よくあるご質問（準備中）</div>
+          <div className="text-lg">
+            <div className="collapse collapse-arrow">
+              <input type="checkbox" className="peer" />
+              <div className="collapse-title pl-0">
+                <div>総選挙内企画</div>
+              </div>
+              <div className="collapse-content">
+                <div className="py-2 link link-hover underline-offset-4">
+                  <Link href="/events-in-event">総選挙内企画について</Link>
+                </div>
+                <div>
+                  <ul className="list-decimal pl-8">
+                    <li className="py-2">
+                      <div className="text-base link link-hover underline-offset-4">
+                        <Link href="/events-in-event/election-campaign">
+                          選挙運動
+                        </Link>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="text-base">開票イラスト（準備中）</div>
+                    </li>
+                    <li className="py-2">
+                      <div className="text-base link link-hover underline-offset-4">
+                        <Link href="/events-in-event/novels-on-themes">
+                          お題小説
+                        </Link>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="text-base">推し台詞（準備中）</div>
+                    </li>
+                    <li className="py-2">
+                      <div className="text-base link link-hover underline-offset-4">
+                        <Link href="/events-in-event/op-cl-illustrations">
+                          OP&CLイラスト
+                        </Link>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="text-base">ボーナス票（準備中）</div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="text-lg">
             <PreviousWebsites />
           </div>
---
+++ pages/events-in-event/election-campaign.tsx
@@ -0,0 +1,55 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+import HumbergerNavigation from '../../components/HumbergerMenu/HumbergerNavigation'
+import NavBar from '../../components/NavBar'
+import SiteFooter from '../../components/SiteFooter'
+
+const ElectionCampaign: NextPage = () => {
+  return (
+    <div className="bg-white text-black">
+      <title>選挙運動 - 幻水総選挙2022</title>
+      <div className={'right'}>
+        <HumbergerNavigation />
+      </div>
+
+      <main id="page-wrap">
+        <NavBar />
+
+        <div className="text-base bg-gray-700 text-white breadcrumbs pl-6 pb-2">
+          <ul>
+            <li>
+              <Link href="/">ホーム</Link>
+            </li>
+            <li>
+              <Link href="/events-in-event">総選挙内企画</Link>
+            </li>
+            <li>選挙運動</li>
+          </ul>
+        </div>
+
+        <div className="hero">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-2xl font-bold pb-4">① 選挙運動</h1>
+              <div className="text-base text-left">
+                <ul className="list-disc pl-6 pr-2">
+                  <li className="pb-2">
+                    ハッシュタグ「 #幻水総選挙運動
+                    」をつけて、選挙運動をしませんか？
+                    推しを応援するも良し。魅力や好きなシーンを語って布教するも良し。総選挙を盛り上げるも良し。ご自由にお使い下さい。
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="divider" />
+        <SiteFooter />
+      </main>
+    </div>
+  )
+}
+
+export default ElectionCampaign

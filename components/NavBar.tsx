6cc67300cbebdc6912488011de32cbaf59c213e6
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 18:19:59 +0900

    feat: 🎸 NavBar 切り出し時に sticky の追尾が削除されてしまっていたので復活した

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const NavBar: NextPage = () => {
   return (
     <>
-      <div className="navbar bg-neutral text-white">
+      <div className="navbar bg-neutral text-white  sticky top-0 z-50">
         <div className="text-xl pl-4 font-zen-old-mincho">
           <Link href="/">幻水総選挙2022</Link>
         </div>

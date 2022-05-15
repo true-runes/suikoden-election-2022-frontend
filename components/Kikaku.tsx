df286b8f08784dca4b444620cb4e6a2e8d05471c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 12:35:55 +0900

    feat: 🎸 2022/05/15昼リリース用の最終調整

--- components/Kikaku.tsx
+++ components/Kikaku.tsx
@@ -8,16 +8,18 @@ const Kikaku: NextPage = () => {
           <div className="max-w-md">
             <h1 className="text-2xl font-bold pb-4">総選挙内企画</h1>
             <p className="pb-4">
-              今年も下記を募集します。詳細は後日発表いたします。
+              今年も下記の企画を準備中です。詳細は後日発表いたします。
             </p>
             <div className="text-base text-left pb-4">
               <ul className="list-decimal pl-6 pr-2">
+                <li className="pb-2">選挙運動</li>
                 <li className="pb-2">開票イラスト</li>
                 <li className="pb-2">お題小説</li>
                 <li className="pb-2">推し台詞</li>
                 <li className="pb-2">
                   オープニングイラスト・クロージングイラスト
                 </li>
+                <li className="pb-2">ボーナス票</li>
               </ul>
             </div>
           </div>

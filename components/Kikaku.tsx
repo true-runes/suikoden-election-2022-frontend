70c5458fca3638bf16744c5eff3245141153f137
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 22:14:48 +0900

    feat: 🎸 「総選挙内企画」のページを20220515リリースにあわせて正式実装した (#251)

--- components/Kikaku.tsx
+++ components/Kikaku.tsx
@@ -17,21 +17,27 @@ const Kikaku: NextPage = () => {
               </p>
               <div className="text-base text-left pb-4">
                 <ul className="list-decimal pl-6 pr-2">
-                  <li className="pb-2 link link-hover underline-offset-4">
+                  <li className="pb-4 underline link link-hover underline-offset-4">
                     <Link href="/events-in-event/election-campaign">
-                      選挙運動
+                      <span className="underline underline-offset-4 text-blue-500 hover:text-blue-900">
+                        選挙運動
+                      </span>
                     </Link>
                   </li>
-                  <li className="pb-2">開票イラスト（準備中）</li>
-                  <li className="pb-2 link link-hover underline-offset-4">
+                  <li className="pb-4">開票イラスト（準備中）</li>
+                  <li className="pb-4 underline link link-hover underline-offset-4">
                     <Link href="/events-in-event/novels-on-themes">
-                      お題小説
+                      <span className="underline underline-offset-4 text-blue-500 hover:text-blue-900">
+                        お題小説
+                      </span>
                     </Link>
                   </li>
-                  <li className="pb-2">推し台詞（準備中）</li>
-                  <li className="pb-2 link link-hover underline-offset-4">
+                  <li className="pb-4">推し台詞（準備中）</li>
+                  <li className="pb-4 underline link link-hover underline-offset-4">
                     <Link href="/events-in-event/op-cl-illustrations">
-                      オープニングイラスト・クロージングイラスト
+                      <span className="underline underline-offset-4 text-blue-500 hover:text-blue-900">
+                        オープニングイラスト・クロージングイラスト
+                      </span>
                     </Link>
                   </li>
                   <li className="pb-2">ボーナス票（準備中）</li>
--- pages/events-in-event/op-cl-illustrations.tsx
+++ pages/events-in-event/op-cl-illustrations.tsx
@@ -24,7 +24,7 @@ const OpClIllustrations: NextPage = () => {
             <li>
               <Link href="/events-in-event">総選挙内企画</Link>
             </li>
-            <li>OPイラスト・CLイラスト</li>
+            <li>OP&CLイラスト</li>
           </ul>
         </div>
 
@@ -34,14 +34,67 @@ const OpClIllustrations: NextPage = () => {
               <h1 className="text-2xl font-bold pb-4">
                 OPイラスト・CLイラスト
               </h1>
+              <h2 className="text-xl font-bold pb-4">OP＆CLイラストとは？</h2>
+              <div className="text-base text-left pb-4">
+                オープニングイラスト（OPイラスト）・クロージングイラスト（CLイラスト）は、それぞれ「投票開始」、「企画終了」のツイート時に掲載させて頂くイラストです。
+                ご協力頂ける方はDMでご連絡下さい。
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">募集内容の詳細</h2>
               <div className="text-base text-left">
                 <ul className="list-disc pl-6 pr-2">
                   <li className="pb-2">
-                    ハッシュタグ「 #幻水総選挙運動
-                    」をつけて、選挙運動をしませんか？
-                    推しを応援するも良し。魅力や好きなシーンを語って布教するも良し。総選挙を盛り上げるも良し。ご自由にお使い下さい。
+                    DMの送り先は{' '}
+                    <a
+                      href="https://twitter.com/gensosenkyo"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="underline underline-offset-4 text-blue-500 hover:text-blue-900"
+                    >
+                      @gensosenkyo
+                    </a>{' '}
+                    となります。
+                  </li>
+                  <li className="pb-2">
+                    <p>使用時にお名前とtwitter IDを掲載させて頂きます。</p>
+                    <p>（ID掲載を希望されない場合、お名前のみ記載致します）</p>
+                  </li>
+                  <li className="pb-2">イラストに文字を入れさせて頂きます。</li>
+                  <li className="pb-2">
+                    キャラや内容に指定はございません。ご自由にお描きください。
+                  </li>
+                  <li className="pb-2">
+                    <p>
+                      ご寄稿いただきましたイラストは、ご自身のSNSや同人誌への掲載などご自由にして頂いて構いません。
+                    </p>
+                    <p>
+                      （可能であれば、総選挙終了後の公開としていただけますと幸いです）
+                    </p>
+                  </li>
+                  <li className="pb-2">
+                    <p>
+                      来年以降の総選挙の宣伝に使わせていただく可能性がございます。
+                    </p>
+                    <p>（その場合は改めてご連絡します）</p>
                   </li>
                 </ul>
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">お礼</h2>
+              <div className="text-base text-left">
+                OPイラスト・CLイラストの作者様には、お礼として各部門「+1票」の追加投票権を進呈いたします。
+              </div>
+
+              <div className="divider" />
+
+              <h2 className="text-xl font-bold pb-4">イラスト送付〆切</h2>
+              <div className="text-base text-center">
+                <p className="text-red-500 pb-1">2022年6月12日（日）</p>
+                <p>（ご相談に応じます）</p>
               </div>
             </div>
           </div>

4c19e805ef3756190ffdd1e98e8f74df54b7242c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 15:12:26 +0900

    feat: 🎸 「イラストご協力」コンポーネントにアカウント名とそこへのリンクを追加した

--- components/IllustratedBy.tsx
+++ components/IllustratedBy.tsx
@@ -3,7 +3,18 @@ import type { NextPage } from 'next'
 const IllustratedBy: NextPage = () => {
   return (
     <>
-      <div className="text-base text-center">イラストご協力: まみた様</div>
+      <div className="text-base text-center">
+        <p className="pb-1">イラストご協力</p>
+        まみた様 (
+        <a
+          href="https://twitter.com/mamita404"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="underline">@mamita404</span>
+        </a>
+        )
+      </div>
     </>
   )
 }

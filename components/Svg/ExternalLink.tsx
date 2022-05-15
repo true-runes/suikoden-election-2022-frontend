db9c36b0618653f4bbbdd3ccad60c102b2e30b68
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 20:06:14 +0900

    feat: 🎸 Twitter や ExternalLink のアイコンをコンポーネントに切り出してリファクタした

---
+++ components/Svg/ExternalLink.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next'
+
+const ExternalLink: NextPage = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+        style={{
+          display: 'inline-block',
+          verticalAlign: 'text-bottom',
+        }}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+        />
+      </svg>
+    </>
+  )
+}
+
+export default ExternalLink

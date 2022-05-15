eefdac134942da769c197163a45f9203ed471160
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 19:15:25 +0900

    feat: 🎸 ハンバーガーメニュー内の外部リンクのアイコン表示と挙動、列スタイルなどを実装 (#243)

--- components/HumbergerMenu/PreviousWebsiteLink.tsx
+++ components/HumbergerMenu/PreviousWebsiteLink.tsx
@@ -1,8 +1,35 @@
 const PreviousWebsiteLink = (props: any) => {
   return (
     <p className="py-2 pl-4">
-      <a href={props.href} target="_blank" rel="noreferrer">
-        {props.siteName}{' '}
+      <a
+        href={props.href}
+        target="_blank"
+        rel="noreferrer"
+        className="link link-hover"
+      >
+        <div className="flex">
+          <div className="pr-0 flex-auto">{props.siteName}</div>
+          <div className="flex-initial w-14">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              style={{
+                display: 'inline-block',
+                verticalAlign: 'text-bottom',
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          </div>
+        </div>
       </a>
     </p>
   )

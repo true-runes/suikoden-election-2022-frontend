d5b31821d1b07c667437e9b050abc1e4eaf6333d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-15 16:42:31 +0900

    feat: 🎸 NavBar コンポーネント を切り出した (#237)

---
+++ components/NavBar.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+const NavBar: NextPage = () => {
+  return (
+    <>
+      <div className="navbar bg-neutral text-white">
+        <div className="text-xl pl-4">
+          <Link href="/">幻水総選挙2022</Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NavBar

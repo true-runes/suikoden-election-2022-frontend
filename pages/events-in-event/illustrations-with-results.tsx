89f94b02e855550396222e5f15d2daa43bbae35e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-24 00:55:42 +0900

    feat: 🎸 パンくずリストを固定した

--- pages/events-in-event/illustrations-with-results.tsx
+++ pages/events-in-event/illustrations-with-results.tsx
@@ -21,7 +21,7 @@ const IllustrationsWithResults: NextPage = () => {
       <main id="page-wrap">
         <NavBar />
 
-        <div className="text-base bg-gray-700 text-white breadcrumbs pl-6 pb-2">
+        <div className="text-base bg-gray-700 text-white breadcrumbs pl-6 pb-2 sticky top-16 z-50">
           <ul>
             <li>
               <Link href="/">{t.PAGE_TITLE_HOME}</Link>

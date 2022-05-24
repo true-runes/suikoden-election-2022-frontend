ab04ecb3eb703a921739e56aff32de2f398120ea
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-25 00:35:38 +0900

    style: 💄 Sider の指摘に従い記法を修正

--- deploy_to_railway_development.sh
+++ deploy_to_railway_development.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
-$HOME/.nodenv/shims/yarn build
-$HOME/.nodenv/shims/railway up --service "幻水総選挙2022 (development)"
+"$HOME"/.nodenv/shims/yarn build
+"$HOME"/.nodenv/shims/railway up --service "幻水総選挙2022 (development)"
 
 exit 0

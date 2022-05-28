5c1acc882f27973341ce1b6d84915e3bdc7c345e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-28 16:56:36 +0900

    feat: 🎸 主催宛DM文言修正

--- pages/events-in-event/illustrations-with-results.tsx
+++ pages/events-in-event/illustrations-with-results.tsx
@@ -254,18 +254,26 @@ const IllustrationsWithResults: NextPage = () => {
                       ② 主催宛DMで送信
                     </span>
                   </h3>
-                  <a
-                    href="https://twitter.com/gensosenkyo"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="link link-hover underline-offset-4"
-                  >
-                    <span className="underline underline-offset-4 text-blue-500 hover:text-blue-900 pl-1 pr-1">
-                      主催
-                    </span>
-                  </a>
-                  宛DM でファイルの送信をお願いします。
+                  <ul className="list-disc pl-6 pr-2">
+                    <li className="pb-2">
+                      <a
+                        href="https://twitter.com/gensosenkyo"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="link link-hover underline-offset-4"
+                      >
+                        <span className="underline underline-offset-4 text-blue-500 hover:text-blue-900 pl-1 pr-1">
+                          主催
+                        </span>
+                      </a>
+                      宛DM でファイルの送信をお願いします。
+                    </li>
+                    <li className="pb-2">
+                      Twitterの仕様につき、画像が劣化する場合がありますのでご注意ください。
+                    </li>
+                  </ul>
                 </div>
+
                 <></>
                 <></>
                 <></>

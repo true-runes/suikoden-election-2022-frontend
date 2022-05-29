a9ad15aedf1c4c87315747bb02bc1c45bd423ee1
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-29 19:08:42 +0900

    feat: 🎸 list-none の padding-left を調整した

--- pages/events-in-event/sosenkyo-book.tsx
+++ pages/events-in-event/sosenkyo-book.tsx
@@ -130,7 +130,7 @@ const SosenkyoBook: NextPage = () => {
                     <ul className="list-disc pl-6 pr-2">
                       <li className="pb-2">
                         <div className="pb-2">縦長の場合</div>
-                        <ul className="list-none pl-6 pr-2">
+                        <ul className="list-none pl-0 pr-2">
                           <li className="pb-2">
                             <div>幅 1100px × 高さ 1500px 以上</div>
                             <div>幅 2508px × 高さ 3541px 以下</div>
@@ -139,7 +139,7 @@ const SosenkyoBook: NextPage = () => {
                       </li>
                       <li className="pb-2">
                         <div className="pb-2">横長の場合</div>
-                        <ul className="list-none pl-6 pr-2">
+                        <ul className="list-none pl-0 pr-2">
                           <li className="pb-2">
                             <div>幅 1500px × 高さ 1100px 以上</div>
                             <div>幅 3541px × 高さ 2508px 以下</div>
@@ -148,7 +148,7 @@ const SosenkyoBook: NextPage = () => {
                       </li>
                       <li className="pb-2">
                         <div className="pb-2">正方形の場合</div>
-                        <ul className="list-none pl-6 pr-2">
+                        <ul className="list-none pl-0 pr-2">
                           <li className="pb-2">
                             <div>幅 1300px × 高さ 1300px 以上</div>
                           </li>

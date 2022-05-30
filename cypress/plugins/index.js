2d5f28bb5f40c683df7a80cb0c06dae161a3e219
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-31 00:10:01 +0900

    test: 💍 Cypress の Spec を追加した (#342)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,11 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('task', {
+    log(message) {
+      console.log(`[cy.log] ${message}`)
+
+      return null
+    },
+  })
 }

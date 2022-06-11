516a15f7f96bd2f02d0308aed6c26e7800fc07a8
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-11 14:16:35 +0900

    feat: 🎸 「ボーナス票が入る部門」の説明を追加した (#389)

--- pages/events-in-event/bonus-votes.tsx
+++ pages/events-in-event/bonus-votes.tsx
@@ -189,6 +189,21 @@ const BonusVotes: NextPage = () => {
 
               <div className="divider" />
 
+              <h2 className="text-xl font-bold pb-4">ボーナス票が入る部門</h2>
+              <div className="text-base text-center pb-0">
+                <div className="pb-4">
+                  <div className="text-base text-center pb-4">
+                    ボーナス票は
+                    <span className="text-red-500">
+                      「オールキャラ部門」の票数
+                    </span>
+                    に反映されます。
+                  </div>
+                </div>
+              </div>
+
+              <div className="divider" />
+
               <h2 className="text-xl font-bold pb-4">ボーナス票の例</h2>
               <div className="text-base text-center pb-0">
                 <div className="pb-4">

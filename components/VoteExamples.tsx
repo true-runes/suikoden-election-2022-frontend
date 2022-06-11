89d768144d47e04dbfafe695e06b4bf4782193ee
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-11 20:54:26 +0900

    feat: 🎸 「投票例」のコンポーネントを作成した (#397)
    * feat: 🎸 「投票例」のコンポーネントを作成した
    
    * fix: 🐛 投票例の文言を修正
    
    * feat: 🎸 投票ツイート注意事項を赤字にした
    
    * test: 💍 E2Eテストを修正した

---
+++ components/VoteExamples.tsx
@@ -0,0 +1,95 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import { useLocale } from '../hooks/useLocale'
+
+const VoteExamples: NextPage = () => {
+  const { locale } = useLocale()
+
+  return (
+    <div className="bg-white text-black">
+      <div className="hero">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-2xl font-bold pb-10 underline font-zen-old-mincho">
+              {locale === 'ja' ? '投票例' : '投票例'}
+            </h1>
+
+            <div className="text-base text-center pb-6">
+              <h2 className="text-xl font-bold pb-4">
+                オールキャラ部門 投票例①
+              </h2>
+              <Image
+                src="/vote_example_all_chara_01.png"
+                alt="オールキャラ部門 投票例 01"
+                width={1166}
+                height={336}
+              />
+            </div>
+
+            <div className="text-base text-center pb-6">
+              <h2 className="text-xl font-bold pb-4">
+                オールキャラ部門 投票例②
+              </h2>
+              <Image
+                src="/vote_example_all_chara_02.png"
+                alt="オールキャラ部門 投票例 02"
+                width={1176}
+                height={480}
+              />
+            </div>
+
+            <div className="text-base text-center pb-6">
+              <h2 className="text-xl font-bold pb-4">コンビ部門 投票例①</h2>
+              <Image
+                src="/vote_example_combination_01.png"
+                alt="コンビ部門 投票例 01"
+                width={1182}
+                height={318}
+              />
+            </div>
+
+            <div className="text-base text-center pb-6">
+              <h2 className="text-xl font-bold pb-4">コンビ部門 投票例②</h2>
+              <Image
+                src="/vote_example_combination_02.png"
+                alt="コンビ部門 投票例 02"
+                width={1180}
+                height={370}
+              />
+            </div>
+
+            <div className="text-base text-left pb-0">
+              <div className="text-base text-left pb-4">
+                ハッシュタグの前には半角スペースまたは改行が必要です。
+                <br />
+                キャラやコンビがしっかりと判別できれば、書式は問いません。
+              </div>
+              <ul className="list-disc pl-6 pr-2">
+                <li className="pb-2">
+                  <span className="text-red-500">
+                    オールキャラ部門とコンビ部門は、それぞれ別のツイートで投票してください。
+                  </span>
+                </li>
+                <li className="pb-2">
+                  愛称を使わずキャラクター名で投票してください。
+                  <br />
+                  （「様」などはOK）
+                </li>
+                <li className="pb-2">
+                  主人公は愛称で投票しても構いません。
+                  <br />
+                  （5主人公→王子、TK主人公→団長　など）
+                </li>
+                <li className="pb-2">
+                  宿星外のキャラにも投票できます。幻水に関係するものであればフリーダムにどうぞ。
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VoteExamples

9fb5a744a544633f9202a0e79edb3e5194a97e9e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-13 20:23:48 +0900

    feat: 🎸 Bugsnag を導入した

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,22 @@ import GoogleTagManager, {
   GoogleTagManagerId,
 } from '@/components/common/GoogleTagManager'
 
+import React from 'react'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
 import '@/styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const thisApiKey: string = process.env.BUGSNAG_API_KEY || ''
+
+  if (thisApiKey !== '') {
+    Bugsnag.start({
+      apiKey: thisApiKey,
+      plugins: [new BugsnagPluginReact(React)],
+    })
+  }
+
   return (
     <>
       <GoogleTagManager

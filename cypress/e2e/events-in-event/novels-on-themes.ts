0fc7b59e5f21930a524d3a63e714bc86ebc1902f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-13 14:39:11 +0900

    test: 💍 未記述であったE2Eテストを追加した

---
+++ cypress/e2e/events-in-event/novels-on-themes.ts
@@ -0,0 +1,60 @@
+describe('ホーム - 総選挙内企画 - お題小説', () => {
+  const targetPath = '/events-in-event/novels-on-themes'
+
+  beforeEach(() => {
+    cy.visit(targetPath)
+  })
+
+  it('ページタイトルが期待どおりであること', () => {
+    cy.url().should('equal', `http://localhost:3100${targetPath}`)
+
+    cy.get('title').should('have.text', 'お題小説 - 幻水総選挙2022')
+  })
+
+  it('パンくずリスト が期待どおりであること', () => {
+    cy.get('.breadcrumbs').should('have.text', 'ホーム総選挙内企画お題小説')
+  })
+
+  it('h1 タグ が期待どおりであること', () => {
+    cy.get('h1').should('have.length', 1)
+    cy.get('h1').then(($h1) => {
+      cy.wrap($h1).eq(0).should('have.text', 'お題小説')
+    })
+  })
+
+  it('h2 タグ が期待どおりであること', () => {
+    cy.get('h2').should('have.length', 11)
+
+    cy.get('h2').then(($h2) => {
+      const texts = $h2.map((_index, item) => {
+        // 各要素に対して jQuery の text() を実行している
+        return Cypress.$(item).text()
+      })
+
+      // jQuery のラップを外して単純な Array にする
+      const gotTexts = texts.get()
+
+      const expectedTexts = [
+        'お題小説とは？',
+        'お題',
+        '文字数',
+        '応募数',
+        '締切',
+        'ツイート例',
+        '応募の受理',
+        '小説の作成方法',
+        '注意点',
+        '作品のご紹介について',
+        '非公開アカウントの方へ',
+      ]
+
+      // Cypress のオブジェクトではないので expect を用いて比較している
+      // そして配列なので deep.equal する
+      expect(gotTexts).to.deep.equal(expectedTexts)
+    })
+  })
+
+  it('フッタ が期待どおりであること', () => {
+    cy.get('footer').should('have.length', 1)
+  })
+})
---
+++ cypress/e2e/events-in-event/oshi-serif.ts
@@ -0,0 +1,55 @@
+describe('ホーム - 総選挙内企画 - 推し台詞教えて！', () => {
+  const targetPath = '/events-in-event/oshi-serif'
+
+  beforeEach(() => {
+    cy.visit(targetPath)
+  })
+
+  it('ページタイトルが期待どおりであること', () => {
+    cy.url().should('equal', `http://localhost:3100${targetPath}`)
+
+    cy.get('title').should('have.text', '推し台詞教えて！ - 幻水総選挙2022')
+  })
+
+  it('パンくずリスト が期待どおりであること', () => {
+    cy.get('.breadcrumbs').should(
+      'have.text',
+      'ホーム総選挙内企画推し台詞教えて！'
+    )
+  })
+
+  it('h1 タグ が期待どおりであること', () => {
+    cy.get('h1').should('have.length', 1)
+    cy.get('h1').then(($h1) => {
+      cy.wrap($h1).eq(0).should('have.text', '推し台詞教えて！')
+    })
+  })
+
+  it('h2 タグ が期待どおりであること', () => {
+    cy.get('h2').should('have.length', 4)
+
+    cy.get('h2').then(($h2) => {
+      const texts = $h2.map((_index, item) => {
+        // 各要素に対して jQuery の text() を実行している
+        return Cypress.$(item).text()
+      })
+
+      // jQuery のラップを外して単純な Array にする
+      const gotTexts = texts.get()
+
+      const expectedTexts = [
+        '推し台詞教えて！とは？',
+        '詳細',
+        '募集締切',
+        'ツイート例',
+      ]
+      // Cypress のオブジェクトではないので expect を用いて比較している
+      // そして配列なので deep.equal する
+      expect(gotTexts).to.deep.equal(expectedTexts)
+    })
+  })
+
+  it('フッタ が期待どおりであること', () => {
+    cy.get('footer').should('have.length', 1)
+  })
+})

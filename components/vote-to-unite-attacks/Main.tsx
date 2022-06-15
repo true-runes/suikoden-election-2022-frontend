7c78fe243efc824326314153267ba2afa611e44e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-15 21:16:59 +0900

    feat: 🎸 雛形実装OK

---
+++ components/vote-to-unite-attacks/Main.tsx
@@ -0,0 +1,128 @@
+import type { NextPage } from 'next'
+
+export const Main: NextPage = () => {
+  // TODO: ソートする
+  const rhapsodiaItems = [
+    '忍者攻撃',
+    '海賊攻撃',
+    '親子攻撃',
+    '付き人攻撃',
+    'お姫様攻撃',
+    '海賊親子攻撃',
+    '麗しの友情攻撃',
+    '新・騎士団攻撃',
+    'アイスクラッシュ攻撃',
+    '美青年攻撃',
+    '美女攻撃',
+    '美少女攻撃',
+    '真・忍者攻撃',
+    'ニャー？攻撃',
+    '乱れ撃ち攻撃',
+    '槍攻撃',
+    '腐れ縁攻撃',
+    '大剣攻撃',
+    'オベル軍攻撃',
+    '遺志を継ぐもの攻撃',
+    '元・騎士団攻撃',
+    '海族団攻撃',
+    '魔法使い攻撃',
+    '火炎陣',
+    '雷神',
+    '風烈牙',
+    '水竜',
+    '焦土',
+    'Ｗリーダー攻撃',
+    '双剣攻撃',
+    '真・友情攻撃',
+    '飛行部隊攻撃',
+    '騎馬部隊攻撃',
+  ]
+
+  return (
+    <>
+      <div className="bg-white text-black">
+        <div className="hero">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-2xl font-bold pb-10 underline font-zen-old-mincho">
+                「協力攻撃部門」投票テンプレ
+              </h1>
+
+              <div className="pb-0">
+                <ul className="list-disc text-left pl-6 pr-2">
+                  <li className="pb-2">たぶん説明が入る</li>
+                  <li className="pb-2">たぶん説明が入る</li>
+                  <li className="pb-2">たぶん説明が入る</li>
+                </ul>
+              </div>
+
+              <div className="divider" />
+
+              {/* TODO: コンポーネント化する */}
+              <div className="pb-10">
+                <h2 className="text-xl font-bold pb-8 text-center">作品名</h2>
+                <div>
+                  <select className="select select-accent w-full max-w-xs bg-white text-black">
+                    <option disabled selected>
+                      作品名を選択して下さい
+                    </option>
+                    {/* ここはベタ書きでいい。ここにレンダリングコストを使いたくない */}
+                    <option>幻想水滸伝</option>
+                    <option>幻想水滸伝II</option>
+                    <option>幻想水滸伝III</option>
+                    <option>幻想水滸伝IV</option>
+                    <option>ラプソディア</option>
+                    <option>幻想水滸伝V</option>
+                    <option>幻想水滸伝ティアクライス</option>
+                    <option>幻想水滸伝 紡がれし百年の時</option>
+                  </select>
+                </div>
+              </div>
+
+              {/* TODO: コンポーネント化する */}
+              <div className="pb-10">
+                <h2 className="text-xl font-bold pb-8 text-center">
+                  協力攻撃名
+                </h2>
+                <div>
+                  <div className="pb-0 w-full">
+                    {/* TODO: コンポーネント化する。作品名に応じて渡す props を変えれば良さそう */}
+                    <ul className="list text-left pl-6 pr-2 w-full">
+                      {rhapsodiaItems.map((item, index) => (
+                        <li key={index} className="pb-2">
+                          <input
+                            id={`${item}`}
+                            type="checkbox"
+                            className="checkbox checkbox-accent mr-4 pb-4"
+                          />
+                          <label htmlFor={`${item}`} className="checkbox-label">
+                            <span className="align-top">{item}</span>
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              {/* TODO: コンポーネント化する */}
+              <div className="pb-10">
+                {/* TODO: 押下条件の設定（バリデーション） */}
+                {/* TODO: ツイート内容の動的生成 */}
+                <a
+                  href="https://twitter.com/intent/tweet?text=テストツイートです"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn btn-outline btn-secondary">
+                    ツイートの雛形を生成する（ツイッターアプリへ）
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ pages/vote-to-unite-attacks.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+import HumbergerNavigation from '@/components/humberger-menu/HumbergerNavigation'
+import { NavBar } from '@/components/common/NavBar'
+import { SiteFooter } from '@/components/common/SiteFooter'
+
+import { Main } from '@/components/vote-to-unite-attacks/Main'
+
+const EventsInEvent: NextPage = () => {
+  return (
+    <div className="bg-white text-black">
+      <title>「協力攻撃部門」投票テンプレ - 幻水総選挙2022</title>
+      <div className="right">
+        <HumbergerNavigation />
+      </div>
+
+      <main id="page-wrap">
+        <NavBar />
+
+        <div className="text-base bg-gray-700 text-white breadcrumbs pl-6 pb-2 sticky top-16 z-50">
+          <ul>
+            <li>
+              <Link href="/" passHref>
+                ホーム
+              </Link>
+            </li>
+            <li>「協力攻撃部門」投票テンプレ</li>
+          </ul>
+        </div>
+
+        <Main />
+
+        <div className="divider" />
+        <SiteFooter />
+      </main>
+    </div>
+  )
+}
+
+export default EventsInEvent

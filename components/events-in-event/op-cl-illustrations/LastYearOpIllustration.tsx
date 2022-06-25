efb7c4e51799a2e2df557d1ec3fd2a699aac11f4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-25 14:11:58 +0900

    feat: 🎸 TwitterTweetEmbed すべてに「読み込み中…」のプレースホルダを与えた

--- components/events-in-event/op-cl-illustrations/LastYearOpIllustration.tsx
+++ components/events-in-event/op-cl-illustrations/LastYearOpIllustration.tsx
@@ -1,15 +1,13 @@
 import type { NextPage } from 'next'
 import { TwitterTweetEmbed } from 'react-twitter-embed'
-// import useTranslation from 'next-translate/useTranslation'
 
 export const LastYearOpIllustration: NextPage = () => {
-  // const { t } = useTranslation('')
-
   return (
     <>
       <h2 className="text-xl font-bold pb-4">昨年のOPイラスト</h2>
       <div className="text-base">
         <TwitterTweetEmbed
+          placeholder={'読み込み中…'}
           tweetId="1403321117125775360"
           options={{ id: 'gensosenkyo', lang: 'ja' }}
         />
